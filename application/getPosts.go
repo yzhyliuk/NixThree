1ff570a99d8921327169f8aacb73343660971797
Yaroslav <y.zhyliuk@gmail.com>
2021-01-17 18:24:09 +0200

    Added methods to "parseApp" type for parsing Comments and Posts

---
+++ application/getPosts.go
@@ -0,0 +1,29 @@
+package application
+
+import (
+	"NixTwo/data/post"
+	"encoding/json"
+	"net/http"
+	"fmt"
+)
+
+
+func (pa *parseApp) getPostByUserID(userID int) {
+		url := pa.getPostSourceString(userID)
+		requestPost, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("Error: can't get post of user #%d from url: %s \n", userID, url)
+			return
+		}
+		var postArray []post.Post
+		err = json.NewDecoder(requestPost.Body).Decode(&postArray)
+		if err != nil {
+			fmt.Println("Unable to unmarshal response body")
+			return
+		}
+		for _, pst := range postArray {
+			pa.wg.Add(1)
+			go pa.getComments(pst.ID)
+			//To do
+		}
+}
