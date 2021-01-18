9de2d2be47a96202019c47c39b6919773c577d6f
Yaroslav <y.zhyliuk@gmail.com>
2021-01-18 11:30:45 +0200

    Added Save method for struct
    Method Save uses *gorm.DB connecton from mysql package to create an new row in DB

--- data/comment/comment.go
+++ data/comment/comment.go
@@ -1,9 +1,26 @@
 package comment
 
+import (
+	"NixTwo/dataSources/mysql"
+	"fmt"
+)
+
+var (
+	dbname = "blogbase"
+)
+
 type Comment struct {
-	PostID	int		`json:"postId"`
-	ID 		int		`json:"id"`
-	Name 	string	`json:"name"`
-	Email 	string	`json:"email"`
-	Body 	string	`json:"body"`
+	PostID int    `json:"postId"`
+	ID     int    `json:"id" gorm:"primaryKey"`
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Body   string `json:"body"`
+}
+
+//Save : saves post to database
+func (c Comment) Save() {
+	result := mysql.DataBase.Create(&c)
+	if result.Error != nil {
+		fmt.Println("Can't add an post to database")
+	}
 }
--- data/post/post.go
+++ data/post/post.go
@@ -1,8 +1,26 @@
 package post
 
+import (
+	"NixTwo/dataSources/mysql"
+	"fmt"
+)
+
+var (
+	dbname = "blogbase"
+)
+
+//Post : struct that represents Post
 type Post struct {
-	UserID int		`json:"userId"`
-	ID int			`json:"id"`
-	Title string	`json:"title"`
-	Body string 	`json:"body"`
+	UserID int    `json:"userId"`
+	ID     int    `json:"id" gorm:"primaryKey"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
+
+//Save : saves post to database
+func (p *Post) Save() {
+	result := mysql.DataBase.Create(&p)
+	if result.Error != nil {
+		fmt.Println("Can't add an post to database")
+	}
 }
